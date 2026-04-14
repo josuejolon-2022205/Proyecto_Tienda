c7e7d0f618e62a17ddba350b9e58ba9866de9fda
josuejolon-2022205 <jjolon-2022205@kinal.edu.gt>
2026-04-14 11:48:31 -0600

    refactor: se cambiaron los metodos http para conectar a base de datos junto con un nuevo mapeo

--- ProyectoTienda/src/main/java/com/JosueJolon/ProyectoTienda/Controllers/loginController.java
+++ ProyectoTienda/src/main/java/com/JosueJolon/ProyectoTienda/Controllers/loginController.java
@@ -1,78 +1,124 @@
 package com.JosueJolon.ProyectoTienda.Controllers;
 
+import com.JosueJolon.ProyectoTienda.Entity.Productos;
+import com.JosueJolon.ProyectoTienda.Entity.Usuarios;
+import com.JosueJolon.ProyectoTienda.Repository.UsuariosRepository;
+import com.JosueJolon.ProyectoTienda.Service.ProductosService;
+import com.JosueJolon.ProyectoTienda.Service.UsuariosService;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
+
 @Controller
 public class loginController {
+
+    private final UsuariosService usuariosService;
+    private final ProductosService productosService;
+
+    public loginController(UsuariosService usuariosService, ProductosService productosService) {
+
+        this.usuariosService = usuariosService;
+        this.productosService = productosService;
+    }
+
     @GetMapping("/")
     public String inicio(){
-        return "redirect:/usuario";
+        return "redirect:/login";
     }
 
-    @GetMapping("/usuario")
+    @GetMapping("/login")
     public String mostrarLogin(){
-        return "usuario";
+        return "login";
     }
+
 
     @PostMapping("/login")
-    public String login(@RequestParam String usuario, @RequestParam String password, HttpSession session, Model model){
-        String userCorrecto = "admin";
-        String passCorrecto = "1234";
+    public String validar(@RequestParam("username") String username,
+                          @RequestParam("password") String password,
+                          Model model) {
 
-        if(usuario.equals(userCorrecto) && password.equals(passCorrecto)){
-            session.setAttribute("usuarioLogueado", usuario);
+        Usuarios u = usuariosService.login(username, password);
+
+        if (u != null) {
             return "redirect:/home";
-        }else{
-            model.addAttribute("error", "usuario y contraseña incorrectos");
-            return "usuario";
+        } else {
+            model.addAttribute("error", "Credenciales incorrectas");
+            return "login";
         }
+    }
+
+    // REGISTRO
+    @GetMapping("/registro")
+    public String registro() {
+        return "registro";
+    }
+
+    @PostMapping("/registro")
+    public String guardar(@RequestParam("username") String username,
+                          @RequestParam("password") String password,
+                          Model model) {
+
+        Usuarios u = usuariosService.login(username, password);
+
+        if (u == null) {
+            model.addAttribute("error", "Usuario ya existe");
+            return "registro";
+        }
+
+        return "redirect:/login";
+    }
+
+    // LISTA
+    @GetMapping("/lista")
+    public String listar(Model model) {
+        List<Usuarios> lista = usuariosService.getAListUsuarios();
+        model.addAttribute("usuarios", lista);
+        return "usuarios";
+    }
+
+    @GetMapping("/eliminar/{id}")
+    public String eliminar(@PathVariable  int id) {
+        usuariosService.deleteUsuarios(id);
+        return "redirect:/lista";
     }
 
     @GetMapping("/home")
     public String mostrar(HttpSession session){
 
-        if(session.getAttribute("usuarioLogueado") == null){
-            return "redirect:/usuario";
-        }
         return "home";
     }
 
     @GetMapping("/producto")
-    public String mostrarProducto(HttpSession session){
-        if(session.getAttribute("usuarioLogueado") == null){
-            return "redirect:/usuario";
-        }
+    public String mostrarProducto(Model model){
+        List<Productos> listar = productosService.getAListProductos();
+        model.addAttribute( "productos", listar);
         return "producto";
     }
 
     @GetMapping("/detalleVenta")
     public String mostrarDetalleVenta(HttpSession session){
-        if(session.getAttribute("usuarioLogueado") == null){
-            return "redirect:/usuario";
-        }
         return "detalleVenta";
     }
 
     @GetMapping("/clientes")
     public String mostrarClientes(HttpSession session){
-        if (session.getAttribute("usuarioLogueado") == null){
-            return "redirect:/usuario";
-        }
         return "clientes";
     }
 
     @GetMapping("/ventas")
     public String mostrarVentas(HttpSession session){
-        if(session.getAttribute("usuarioLogueado") == null){
-            return "redirect:/usuario";
-
-        }
         return "ventas";
+    }
+
+    @GetMapping("/usuario")
+    public String mostrarUsuario(HttpSession session){
+        return "usuario";
     }
     
 }
