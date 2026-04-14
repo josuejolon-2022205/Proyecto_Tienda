b05857465ffa329e5f37a0bdb493c6fc67016e49
josuejolon-2022205 <jjolon-2022205@kinal.edu.gt>
2026-04-14 11:50:58 -0600

    feat: se agrego el findByUsername en el JPA

--- ProyectoTienda/src/main/java/com/JosueJolon/ProyectoTienda/Repository/UsuariosRepository.java
+++ ProyectoTienda/src/main/java/com/JosueJolon/ProyectoTienda/Repository/UsuariosRepository.java
@@ -4,4 +4,5 @@ import com.JosueJolon.ProyectoTienda.Entity.Usuarios;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UsuariosRepository extends JpaRepository<Usuarios, Integer> {
+    Usuarios findByUsername(String username);
 }
