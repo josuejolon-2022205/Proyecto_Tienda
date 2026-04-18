56cfbfeba5abba699602cf6b01965cc37a2446c3
josuejolon-2022205 <jjolon-2022205@kinal.edu.gt>
2026-04-17 21:16:51 -0600

    refactor: se cambio la notacion de las excepciones globales. De RestControllerAdvice a ControllerAdvice

--- ProyectoTienda/src/main/java/com/JosueJolon/ProyectoTienda/Exception/GlobalExceptionHandler.java
+++ ProyectoTienda/src/main/java/com/JosueJolon/ProyectoTienda/Exception/GlobalExceptionHandler.java
@@ -5,13 +5,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.List;
 import java.util.Map;
 
-@RestControllerAdvice
+@ControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exceptions.class)
